86bd5e07b1b2770c1d9e1f38c6d585b500883b37
Mustafa21102005 <mustafa21102005@gmail.com>
2026-03-27 20:53:35 +0300

    update: added new projects and removed reddit link

--- src/data/projects.js
+++ src/data/projects.js
@@ -18,4 +18,24 @@ export const projects = [
     tags: ['Laravel', 'Blade', 'CSS', 'JS'],
     link: 'https://github.com/Mustafa21102005/scantrack',
   },
+  {
+    id: 3,
+    title: 'Lectura',
+    category: 'Website',
+    image: '/images/lectura.png',
+    description:
+      'Lectura is a web-based Virtual Learning Environment (VLE) designed to improve how educational institutions manage study materials, assignments, grading, and academic progress.',
+    tags: ['Laravel', 'Blade', 'VLE'],
+    link: 'https://github.com/Mustafa21102005/lectura',
+  },
+  {
+    id: 4,
+    title: 'Book-My-Hotel',
+    category: 'Website',
+    image: '/images/bmh.png',
+    description:
+      'Book-My-Hotel is a hotel reservation platform designed to support sustainable tourism by helping customers easily browse, compare, and book eco-friendly hotels.',
+    tags: ['Laravel', 'Hotel', 'Reservation'],
+    link: 'https://github.com/Mustafa21102005/book-my-hotel',
+  },
 ]

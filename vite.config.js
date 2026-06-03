cd72f96a11c6e380c5825722d6c23cf5089e675b
Mustafa21102005 <mustafa21102005@gmail.com>
2026-06-03 22:47:22 +0300

    update: vite prerender used

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,21 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
+import VitePrerender from 'vite-plugin-prerender'
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePrerender({
+      staticDir: path.join(__dirname, 'dist'),
+
+      routes: ['/'],
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
